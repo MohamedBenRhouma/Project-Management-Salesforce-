58af482e1c8fdfe3297447012bcb531073db09ab
Mohamed Ben Rhouma <mohamed.benrhouma@enis.tn>
2024-05-02 23:40:42 +0100

    Install JDk 11

--- build/run_sfdx_scanner.sh
+++ build/run_sfdx_scanner.sh
@@ -6,3 +6,6 @@ echo -e 'y/n' | sfdx plugins:install @salesforce/sfdx-scanner
 
 echo "Running SFDX Scanner"
 npx sfdx scanner:run --target "**/default/**" --format "csv" --outfile "sfdxScannerAnalysis.csv" --violations-cause-error
+echo "Installing JDK"
+sudo apt update
+sudo apt install openjdk-11-jdk
