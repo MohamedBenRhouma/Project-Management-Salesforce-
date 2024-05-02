9f11408e81dfa93ecc678f395b00531ce84b54bd
Mohamed Ben Rhouma <mohamed.benrhouma@enis.tn>
2024-05-03 00:00:09 +0100

    ADD nODE 18

--- build/run_sfdx_scanner.sh
+++ build/run_sfdx_scanner.sh
@@ -1,10 +1,21 @@
- 
+#!/bin/bash
+
 echo "Installing JDK"
 sudo apt update
 sudo apt install openjdk-11-jdk
 
+echo "Installing Node.js 18 using nvm"
+# Install nvm if not already installed
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+# Activate nvm
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+# Install Node.js 18
+nvm install 18
+
 echo "Install SFDX Scanner"
 echo -e 'y/n' | sfdx plugins:install @salesforce/sfdx-scanner
- 
+
 echo "Running SFDX Scanner"
 npx sfdx scanner:run --target "**/default/**" --format "csv" --outfile "sfdxScannerAnalysis.csv" --violations-cause-error
